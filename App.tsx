7dbdcf5ed0170e496f3e4bcf0b61c7a8c4b2db4d
Gaurav Kakoti <Gauravkakoti834@gmail.com>
2022-10-29 15:16:50 +0530

    🚩

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { NavigationContainer } from '@react-navigation/native';
+import { TailwindProvider } from 'tailwind-rn';
+import RootNavigator from './navigator/RootNavigator';
+import utilities from './tailwind.json';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/api/nobby-quoll',
+  cache: new InMemoryCache(),
+});
+
+export default function App() {
+  return (
+    // @ts-ignore
+    <TailwindProvider utilities={utilities}>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
+    </TailwindProvider>
+  );
+}
